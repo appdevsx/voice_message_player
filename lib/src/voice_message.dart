3026a967e1ab2bf50d4da79ddadfb07ef2149588
yourusernamerabbihossen <joyahmedauthor@gmail.com>
2023-02-25 12:31:18 +0600

    update

--- lib/src/voice_message.dart
+++ lib/src/voice_message.dart
@@ -29,7 +29,7 @@ class VoiceMessage extends StatefulWidget {
     this.onPlay,
   }) : super(key: key);
 
-  final String audioSrc;
+  final audioSrc;
   final int noiseCount;
   final Color meBgColor,
       meFgColor,
@@ -230,19 +230,19 @@ class _VoiceMessageState extends State<VoiceMessage>
 
   _setPlayingStatus() => _isPlaying = _playingStatus == 1;
 
-  _startPlaying() async {
+ void  _startPlaying() async {
     _playingStatus = _player.play(UrlSource(widget.audioSrc)) as int;
     _setPlayingStatus();
     _controller!.forward();
   }
 
-  _stopPlaying() async {
+ void _stopPlaying() async {
     _playingStatus = _player.pause() as int;
     _controller!.stop();
   }
 
   void _setDuration() async {
-    _audioDuration = await jsAudio.AudioPlayer().setUrl(widget.audioSrc);
+    _audioDuration = await jsAudio.AudioPlayer().setUrl((widget.audioSrc));
     duration = _audioDuration!.inSeconds;
     maxDurationForSlider = duration + .0;
 
@@ -294,9 +294,8 @@ class _VoiceMessageState extends State<VoiceMessage>
     _player.dispose();
     super.dispose();
   }
-
   void _listenToRemaningTime() {
-    _player.onPositionChanged.listen((Duration p) {
+    _player.onDurationChanged.listen((Duration p) {
       final _newRemaingTime1 = p.toString().split('.')[0];
       final _newRemaingTime2 =
           _newRemaingTime1.substring(_newRemaingTime1.length - 5);
